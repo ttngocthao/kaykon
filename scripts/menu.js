4d0f017ec1c2f3591168c581b2a07cb6f1058a70
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2019-02-07 23:52:44 +0000

    commit

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,11 +1,4 @@
 $('.menuIcon').on('click',function(){
-    $('.menuList').toggleClass('show');
+    $('.mobile .menuList').toggleClass('show');
 
-    //change icon when menuList showed
-    $(this).find('div:nth-child(1)').toggleClass('closeIcon1');
-    $(this).find('div:nth-child(3)').toggleClass('closeIcon3');
-    $(this).find('div:nth-child(2)').toggleClass('closeIcon2');
-
-    //color layer effect for menuList
-    $('.menuList .effect').toggleClass('effect_active');
 })
