71da49ccbab79aa5580ff248867da3147e88eafe
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2019-02-15 22:29:14 +0000

    added slideShow

---
+++ scripts/slideShow.js
@@ -0,0 +1,14 @@
+let slideIndex=0;
+slideShow();
+function slideShow(){
+    let slides= document.getElementsByClassName('slides');
+    for(var  i=0; i<slides.length; i++){
+        // slides[i].style.display = 'none';
+        slides[i].style.opacity = 0;
+    }
+    slideIndex ++;
+    if(slideIndex > slides.length){slideIndex=1}
+    // slides[slideIndex - 1].style.display= 'block';
+    slides[slideIndex - 1].style.opacity= 1;
+    // setTimeout(slideShow,9000);
+}
